557c6cb5f051ea17613a4d5a16fe0feaa7f9e956
Crypto Gi <41085798+mir1218@users.noreply.github.com>
2023-09-03 02:35:36 -0500

    Update install_tap0_network_service.sh
    Add additional checks
    - if the packages are already installed skip the installation of packages.
    - Show progress of the script

--- install_tap0_network_service.sh
+++ install_tap0_network_service.sh
@@ -1,24 +1,53 @@
 #!/bin/bash
 
+# ANSI escape codes for colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 # File paths
 script_path="/usr/local/bin/setup_network.sh"
 service_path="/etc/systemd/system/setup_network.service"
 
 # Function to execute the network setup script
 execute_setup_network() {
-    echo "Executing network setup script..."
+    echo -e "${GREEN}Executing network setup script... (Step 5/5)${NC}"
     sudo bash $script_path
 }
 
-# Step 1: Check if the network setup script already exists
+# Function to check if a package is installed
+is_package_installed() {
+    dpkg -l "$1" &> /dev/null
+    return $?
+}
+
+echo -e "${GREEN}Initiating Network Setup ...${NC}"
+
+# Step 1: Check for required packages
+echo -e "${GREEN}Checking for required packages... (Step 1/5)${NC}"
+if ! is_package_installed "uml-utilities" || ! is_package_installed "bridge-utils"; then
+    echo -e "${RED}Updating package list...${NC}"
+    sudo apt update -y
+else
+    echo -e "${GREEN}Required packages are already installed. Skipping package list update.${NC}"
+fi
+
+# Step 2: Check if the network setup script already exists
+echo -e "${GREEN}Setting up network script... (Step 2/5)${NC}"
 if [ ! -f $script_path ]; then
+    echo -e "${RED}Creating network setup script...${NC}"
     cat > $script_path << 'EOF'
 #!/bin/bash
 
-# Update package list and install necessary packages
-sudo apt update -y && \
-sudo apt install uml-utilities -y && \
-sudo apt install bridge-utils -y && \
+# Install uml-utilities if not already installed
+if ! is_package_installed "uml-utilities"; then
+    sudo apt install uml-utilities -y
+fi
+
+# Install bridge-utils if not already installed
+if ! is_package_installed "bridge-utils"; then
+    sudo apt install bridge-utils -y
+fi
 
 # Create and configure the network interfaces
 sudo tunctl -t tap0 && \
@@ -35,17 +64,24 @@ sudo ifconfig br0 up && \
 sudo ifconfig tap0 up
 EOF
 
+    echo "is_package_installed() {" >> $script_path
+    echo "    dpkg -l \"\$1\" &> /dev/null" >> $script_path
+    echo "    return \$?" >> $script_path
+    echo "}" >> $script_path
+
     # Make the script executable
     chmod +x $script_path
 
     # Execute the network setup script
     execute_setup_network
 else
-    echo "Network setup script already exists. Skipping..."
+    echo -e "${GREEN}Network setup script already exists. Skipping... (Step 3/5)${NC}"
 fi
 
-# Step 2: Check if the systemd service file already exists
+# Step 4: Check if the systemd service file already exists
+echo -e "${GREEN}Setting up systemd service... (Step 4/5)${NC}"
 if [ ! -f $service_path ]; then
+    echo -e "${RED}Creating systemd service file...${NC}"
     cat > $service_path << 'EOF'
 [Unit]
 Description=Network Setup Script
@@ -62,7 +98,7 @@ EOF
     systemctl daemon-reload
     systemctl enable setup_network.service
 else
-    echo "Systemd service file already exists. Skipping..."
+    echo -e "${GREEN}Systemd service file already exists. Skipping...${NC}"
 fi
 
-echo "Setup complete. The network script will run at boot if not already configured."
+echo -e "${GREEN}Setup complete. The network script will run at boot if not already configured.${NC}"
